9b9ca33532981dd439691cd7da0cebc8f0b0d3f7
Zach Grafton <zachgrafton@gmail.com>
2019-01-15 20:23:38 -0800

    Initial commit

---
+++ app/src/main/java/learningcurve/zachg/criminalintent/MainActivity.java
@@ -0,0 +1,14 @@
+package learningcurve.zachg.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
